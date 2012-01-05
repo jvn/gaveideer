10c9593757534c879c35b096ab86343b579b5edd
jvn <jvn@mikiaholding.dk>
2012-01-05 18:38:22 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gaveideer::Application.config.secret_token = 'd573292bc153472f9f47550c8a94e2e703a69f0ebb0b3db4bcd986f48301bd2ba5bb7fe54e5ec9e3aa5a403959ab55b84e845ec9144bef3257739cee7e1cdf52'
