71442cabf8bcd34eb21d1c27f6ac31e67ed77483
jvn <jvn@mikiaholding.dk>
2012-01-11 21:41:29 +0100

    bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-  bruger.create = Bruger.authenticate(params[:email], params[:password])
-  if user
+  bruger = Bruger.authenticate(params[:email], params[:password])
+  if bruger
+    logger.info('hej')
     session[:bruger_id] = bruger.id
-    redirect_to root_url, :notice => "Le homepage, now logged iN!"
+    redirect_to root_url, :notice => "det virker?"
   else
-    flash.now.alert = "WROnG!!!"
+    flash.now.alert = "forkert kode"
+    render "new"
     end
   end
 end
